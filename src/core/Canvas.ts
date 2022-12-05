623982e7d8f74f09081e1db34f85e5ad464d9fd0
Plume <a.pristchepa@gmail.com>
2022-12-06 00:44:58 +0100

    feat: canvas take parent size by default

--- src/core/Canvas.ts
+++ src/core/Canvas.ts
@@ -3,14 +3,42 @@ import AlpineInstance from "alpinejs";
 
 export default (Alpine: typeof AlpineInstance) => {
   Alpine.directive('canvas', (el, {expression}) => {
+
+    // Create a new Three.js scene
+
     const {scene, camera, renderer} = window.Norska = {
       scene: new THREE.Scene(),
       camera: new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
       renderer: new THREE.WebGLRenderer(),
     }
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    el.appendChild(renderer.domElement);
+    // Create a full width/height canvas
+
+    const parent = document.createElement('div');
+    parent.style.width = '100%';
+    parent.style.height = '100%';
+    (el as HTMLDivElement).insertAdjacentElement('beforebegin', parent);
+
+    const getParentSize = () => {
+      const {width, height} = parent.getBoundingClientRect();
+      return {width, height};
+    }
+
+    const setCanvasSize = () => {
+      const {width, height} = getParentSize();
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderer.setSize( width, height );
+    }
+
+    window.addEventListener( 'resize', setCanvasSize);
+    parent.appendChild(renderer.domElement);
+
+    (el as HTMLDivElement).style.display = 'none';
+
+    setCanvasSize();
+
+    // Start the animation loop
 
     const animate = () => {
       requestAnimationFrame(animate);
--- test/main.ts
+++ test/main.ts
@@ -1,6 +1,6 @@
 import Alpine from "alpinejs";
 import Norska from '../dist/bundle';
 
-Alpine.plugin(Norska);
+Alpine.plugin(Norska as any);
 (window as any).Alpine = Alpine;
 Alpine.start();
