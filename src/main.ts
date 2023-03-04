844fe6d448c402f5c582afbccbb22dc85af118ac
Plume <a.pristchepa@gmail.com>
2023-03-04 23:31:15 +0100

    feat: trial and error

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,9 @@
-import { Camera, Canvas, Load, Scene } from '@/core';
-import { Controls } from '@/controls';
-import { Geometry } from '@/geometries';
-import { Material } from '@/materials';
-import { Light } from '@/lights';
-import { Mesh } from '@/meshes';
-import { Position, Rotation, Scale } from '@/utils';
 import { Frame, N, Three } from '@/magic';
+import * as THREE from 'three';
 
-const directives: Record<string, any> = {
-  camera: Camera,
-  canvas: Canvas,
-  load: Load,
-  controls: Controls,
-  scene: Scene,
-  geometry: Geometry,
-  material: Material,
-  light: Light,
-  mesh: Mesh,
-  position: Position,
-  rotation: Rotation,
-  scale: Scale
-};
+const lowerCaseTHREE = Object.fromEntries(
+  Object.entries(THREE).map(([k, v]) => [k.toLowerCase(), v])
+);
 
 const magics: Record<string, any> = {
   frame: Frame,
@@ -37,8 +20,17 @@ export default (o: Record<string, any>) => {
 
     (Alpine as Alpine).directive(
       options.prefix,
-      (el: any, args: any, routine: any) => {
-        directives[args.modifiers[0]](el, args, routine);
+      (el, { value, modifiers, expression }, { evaluate, effect, cleanup }) => {
+
+        const values = evaluate(expression);
+        console.log(values);
+
+        try {
+          const instance = new (lowerCaseTHREE as any)[modifiers[0]](...values); 
+          console.log(instance);
+        } catch (e) {
+          console.error(`${modifiers[0]}: Invalid THREE.js object`);
+        }
       }
     );
 
