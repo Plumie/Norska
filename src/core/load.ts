507fbc36936b4fdc2deac03fe28a16ba68f4b301
Plume <a.pristchepa@gmail.com>
2023-03-17 18:39:13 +0100

    feat: support new file format (mmd, drc, obj)

--- src/core/load.ts
+++ src/core/load.ts
@@ -1,4 +1,7 @@
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'; 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import {MMDLoader} from 'three/examples/jsm/loaders/MMDLoader';
+import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader';
 
 const Load: AlpineDirective = (
   el,
@@ -10,7 +13,9 @@ const Load: AlpineDirective = (
 
   const createMesh = () => {
     getValues(([file]: string) => {
-      const loader = new GLTFLoader();
+
+      const loader = getLoader(file);
+
       loader.load(file, (gltf) => {
         el._norska.mesh = gltf.scene.children[0];
 
@@ -34,6 +39,22 @@ const Load: AlpineDirective = (
 
   if (!el.hasOwnProperty('_norska')) {
     el._norska = {};
+  }
+
+  const getLoader = (filename: string) => {
+    const extension = filename.split('.').pop();
+    switch (extension) {
+      case 'glb' || 'gltf':
+        return new GLTFLoader();
+      case 'obj':
+        return new OBJLoader();
+      case 'pmd' || 'pmx' || 'vmd' || 'vpd':
+        return new MMDLoader();
+      case 'drc':
+        return new DRACOLoader();
+      default:
+        throw new Error(`Unknown file extension: ${extension}`);
+    }
   }
 
   createMesh();
